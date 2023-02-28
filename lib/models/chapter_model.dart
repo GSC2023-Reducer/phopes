57fdf67a2406c888b8a4ba5160fafbf972ec1b2d
JunBro1908 <hyeang1202@naver.com>
2023-03-01 00:18:56 +0900

    feat detail and view page option and refact 1...10.json files

--- lib/models/chapter_model.dart
+++ lib/models/chapter_model.dart
@@ -1,10 +1,8 @@
 class ChapterModel {
   final String id, name, prev, next, content;
-  bool isRead = false;
   ChapterModel({
     required this.id,
     required this.name,
-    required this.isRead,
     required this.prev,
     required this.next,
     required this.content,
@@ -14,7 +12,6 @@ class ChapterModel {
     return ChapterModel(
       id: jsonData['id'],
       name: jsonData['name'],
-      isRead: jsonData['isRead'],
       prev: jsonData['prev'],
       next: jsonData['next'],
       content: jsonData['content'],
